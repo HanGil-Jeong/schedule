92f13a437ef755f70cd098e4056c6caeb10611e7
HanGil <jhg9240@gmail.com>
2025-03-25 20:53:31 +0900

    feat: ScheduleRequessponse속성 설정

--- src/main/java/com/example/schedule/dto/ScheduleResponseDto.java
+++ src/main/java/com/example/schedule/dto/ScheduleResponseDto.java
@@ -1,4 +1,15 @@
 package com.example.schedule.dto;
 
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
 public class ScheduleResponseDto {
+
+    private Long id;
+    private String todo;
+    private String writersName;
+    private LocalDateTime writtenDate;
+
 }
