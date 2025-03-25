4d52afc7c4435b952487f8744461a1b2135ad13e
HanGil <jhg9240@gmail.com>
2025-03-25 20:35:20 +0900

    feat: Schedule 클래스 속성 작성

--- src/main/java/com/example/schedule/entity/Schedule.java
+++ src/main/java/com/example/schedule/entity/Schedule.java
@@ -1,4 +1,27 @@
 package com.example.schedule.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
 public class Schedule {
+
+    private Long id;
+    private String todo;
+    private String password;
+    private String writersName;
+    private LocalDateTime writtenDate;
+
+
+    public Schedule(String todo, String password, String writersName, LocalDateTime writtenDate){
+
+        this.todo = todo;
+        this.password = password;
+        this.writersName = writersName;
+        this.writtenDate = writtenDate;
+    }
+
 }
